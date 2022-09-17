93c971f8fdde9149e65793e698f280145c9ad9d8
Aditya Kesuma <aditezo24@gmail.com>
2022-09-17 20:24:52 +0700

    Access Modifier

---
+++ Latihan8-Visibility (Access Modifier)/readme.txt
@@ -0,0 +1,12 @@
+Visibility
+-Konsep yang digunakan untuk mengatur akses dari property dan method sebuah objek
+-ada 3 keyword visibility : public, protected dan private
+
+public : dapat digunakan dimana saja, bahkan di luar kelas
+protected : hanya dapat digunakan sebuah kelas beserta turunannya (Inheritance)
+private : hanya dapat digunakan di dalam sebuah kelas tertentu saja
+
+Why?
+- hanya memperlihatkan aspek dari class yang dibutuhkan oleh 'client'
+- Menentukan kebutuhan yang jelas untuk object
+- Memberikan kendali pada kode untuk menghindari 'bug'
---
+++ Latihan8-Visibility (Access Modifier)/visibility.php
@@ -0,0 +1,140 @@
+<?php
+
+// Study Case
+// Jualan Produk
+// Comic
+// Game
+
+class Produk
+{
+    // Property
+    public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit";
+
+    // protected $harga = 0; // tidak bisa di print langsung harus melalui turunan
+
+    protected $diskon = 0;
+    private $harga = 0; // Hanya bisa dipakai pada class ini saja
+
+    // Method Constructor
+    // kenapa menggunakan __ (underline)
+    // dalam php __ berarti adalah method Special
+    // dari PHP
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        // isi dari Construct akan otomatis di jalankan ketika inisiasi Object
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getEdition($edition)
+    {
+        $str = $this->edition = $edition;
+        return $str;
+    }
+
+    // Private method
+    public function getHarga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function getLabel()
+    {
+        // dalam php harus menuliskan $this->VAR
+        // agar mengambil variable diluar method.
+        return "$this->penulis, $this->penerbit";
+    }
+
+    // Inheritance Method
+    public function getInfoProduk()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
+    }
+}
+
+// Child Class
+// Child Kelas akan menggunakan properti dari parent Class
+// Child kelas akan digunakan dahulu variable nya
+// jika tidak ditemukan variablenya maka program akan mencari kedalam parent class
+class Komik extends Produk
+{
+    public $jmlHalaman;
+
+    // Override Method
+    public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman)
+    {
+        parent::__construct(
+            $judul,
+            $penulis,
+            $penerbit,
+            $harga
+        );
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . parent::getInfoProduk() . " (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+// Child Class
+class Game extends Produk
+{
+    public $waktuMain;
+
+    // Override Method
+    public function __construct($judul, $penulis, $penerbit, $harga, $waktuMain)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    // public function getHarga()
+    // {
+    //     return $this->harga;
+    // }
+
+    public function getInfoProduk()
+    {
+        $str =
+            "Game :" . parent::getInfoProduk() . " (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+// Cetak Object Type
+class CetakInfoProduk
+{
+    // Param 1 hanya menerima class yang bernilai Produk
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()}, (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+// Full Property
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+
+// Method Inheritance
+// echo $produk1->getInfoProduk();
+// echo "<br>";
+// echo $produk2->getInfoProduk();
+// echo "<hr>";
+
+echo $produk2->setDiskon(50);
+echo "<br>";
+echo $produk2->getHarga();
